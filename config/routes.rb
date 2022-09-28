f2b7f4c11cbfd8589e46938af17395aad876c491
Akshat Patel <akpatel@deqode.com>
2022-09-28 19:19:57 +0530

    remove: user/new route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'  
   get 'login', to: 'sessions#new', as: 'login'  
   get 'logout', to: 'sessions#destroy', as: 'logout'  
-  resources :users ,expects: [:new, :create, :destroy]
+  resources :users ,except: [:new]
   get "auth/index"
   root "articles#index"
 
