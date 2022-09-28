8d9c1ea6002e2c4cbcd5385510283566a6fff488
Akshat Patel <akpatel@deqode.com>
2022-09-28 19:20:51 +0530

    Add: login validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
-class SessionsController < ApplicationController   
+class SessionsController < ApplicationController
   skip_before_action :login, only: [:new,:create]   
   def new   
   end   
   def create   
-    user = User.find_by_email(params[:email])   
-    if user && user.authenticate(params[:password])   
-      session[:user_id] = user.id   
-      redirect_to root_url
-    else   
-      render :new   
+    user = User.find_by_email(params[:email])
+    if (user)
+      if user && user.authenticate(params[:password])  
+        session[:user_id] = user.id   
+        redirect_to root_url
+      else   
+        flash[:alert] = "Incorrect password"
+        render :new   
+      end
+    else
+      flash[:alert] = "Email not found"
+      redirect_to "/login"
     end   
   end   
   def destroy   
