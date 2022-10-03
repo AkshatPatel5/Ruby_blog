75799f3b2548dc699fab5f724e8d9d6f5522bb76
Akshat Patel <akpatel@deqode.com>
2022-10-03 18:38:12 +0530

    Add: Active storage to store image in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [1000, 1000]
+  end
     has_many :comments
   
     validates :title, presence: true
