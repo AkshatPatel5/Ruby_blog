dfce0cd4b33b219dc07fe06e920e0eb55d20e620
Akshat_7 <akpatel@deqode.com>
2022-11-21 12:02:35 +0530

    Add: Ajax call in while adding comments

---
+++ app/assets/javascripts/article.js
@@ -0,0 +1,38 @@
+//= require jquery3
+//= require jquery_ujs
+$(function () {
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    let commenter = $('#comment_commenter').val();
+    let body = $('#comment_body').val();
+    const url = window.location.pathname;
+    const id = url.substring(url.lastIndexOf('/') + 1);
+    comment = {
+      commenter: commenter,
+      body: body,
+    };
+    $.ajax({
+      type: 'POST',
+      url: `/articles/${id}/comments`,
+      dataType: 'json',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify({ comment }),
+      success: function (data, textStatus, jqXHR) {
+        if (textStatus == 'success') {
+          $('h2:last').before(
+            '<p><strong>Commenter: </strong>' + data.commenter + '</p>',
+            '<p><strong>Comment: </strong>' + data.body + '</p>',
+          );
+        } else {
+          $('body').prepend(`<p>${jqXHR.responseText}</p>`);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $('body').prepend(`<p>${(jqXHR.responseText, errorThrown)}</p>`);
+      },
+    });
+    return false;
+  });
+});
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,10 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    respond_to do |format|
+      format.html { redirect_to article_path(@article) }
+      format.json { render json: comment_params }
+    end
   end
 
   private
