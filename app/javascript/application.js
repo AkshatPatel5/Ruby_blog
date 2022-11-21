2fc9c53f4e62b0e61143c517beea50ddf79b000e
Akshat_7 <akpatel@deqode.com>
2022-11-21 12:03:07 +0530

    trying to add jquery using importmaps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails';
+import 'controllers';
+import jQuery from 'jquery';
+import 'jquery_ujs';
+import 'popper';
+import 'bootstrap';
+
+// NOTE: make jQuery global if needed
+window.$ = window.jQuery = jQuery;
+$(function () {
+  window.alert('j');
+  console.log('j'); // ok
+});
+window.alert('abc');
+import './article';
