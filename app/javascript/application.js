664f295aeaddcccbba6021ed78cc2ff264dd3520
Akshat Patel <akpatel@deqode.com>
2022-11-30 12:43:52 +0530

    Fix: Popper not importing error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails';
 import 'controllers';
-import jQuery from 'jquery';
+import 'jquery';
 import 'jquery_ujs';
 import 'popper';
 import 'bootstrap';
 
-// NOTE: make jQuery global if needed
-window.$ = window.jQuery = jQuery;
-$(function () {
-  window.alert('j');
-  console.log('j'); // ok
-});
-window.alert('abc');
-import './article';
+// import './article';
